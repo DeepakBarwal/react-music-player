96645e3c46da52e0d5e1939a745799b9389afb5c
DeepakBarwal <deepakbarwal98@gmail.com>
2021-02-04 20:34:01 +0530

    library toggle added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
+import Nav from './components/Nav';
 
 // Import Util
 import data from './util';
@@ -23,6 +24,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false);
 
   // Event Handlers
   const timeUpdateHandler = (e) => {
@@ -33,6 +35,7 @@ function App() {
 
   return (
     <div className='App'>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Song currentSong={currentSong} />
       <Player
         audioRef={audioRef}
@@ -48,6 +51,7 @@ function App() {
         setCurrentSong={setCurrentSong}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
       <audio
         onTimeUpdate={timeUpdateHandler}
