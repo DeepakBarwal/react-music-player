aecb07478bbce786a596b03ec854b885acb36c46
DeepakBarwal <deepakbarwal98@gmail.com>
2021-02-04 19:18:16 +0530

    songs can be selected

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong }) => {
+  const songSelectHandler = () => {
+    const selectedSong = song;
+    setCurrentSong(selectedSong);
+  };
+
   return (
-    <div className='library-song'>
+    <div onClick={songSelectHandler} className='library-song'>
       <img src={song.cover} alt={song.name} />
       <div className='song-description'>
         <h3>{song.name}</h3>
